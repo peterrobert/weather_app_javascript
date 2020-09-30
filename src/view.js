d6f27fb71963c0047a5e77770b5c74d7e293e7a3
peterrobert <pwambui93@gmail.com>
2020-09-30 19:37:42 +0300

    created the location view

--- src/view.js
+++ src/view.js
@@ -12,13 +12,35 @@ class View {
     console.log(info)
     let mainContainer = document.getElementById('content');
 
+   //====== class container
+    let divContainer = document.createElement('div');
+    setAttributes(divContainer, {
+      class: "container"
+    })
 
-    let iconContainer = document.createElement('img');
-    setAttributes(iconContainer, {
-      src: `../assets/images/icons/${info.weather[0].icon}.png`,
-    });
+    // ==== row continer
 
-     mainContainer.append(iconContainer)
+    let rowContainer = document.createElement('div');
+    setAttributes(rowContainer, {
+      class: "row"
+    })
+
+
+    let locationIcon = document.createElement('i')
+    setAttributes(locationIcon, {
+      class:"fa fa-map-marker",
+      'aria-hidden': "true"
+    })
+
+    mainContainer.append(locationIcon);
+
+
+    // let iconContainer = document.createElement('img');
+    // setAttributes(iconContainer, {
+    //   src: `../assets/images/icons/${info.weather[0].icon}.png`,
+    // });
+
+    //  mainContainer.append(iconContainer)
    
 
   }
